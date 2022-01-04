6da43a58342b90f544120c19bc89e76ffae73af4
myzb <mc1169502544@outlook.com>
2022-01-04 10:14:27 +0800

    alter harbor

--- install-harbor.sh
+++ install-harbor.sh
@@ -14,9 +14,9 @@ tar -xf ./harbor.tgz  -C /usr/local/
 cd /usr/local/harbor
 mv harbor.yml.tmpl  harbor.yml
 sed -i 's/hostname: reg.mydomain.com/hostname ${hostname}/g' harbor.yml
-sed -i 's/certificate: \/your\/certificate\/path/certificate: ${crtpath}/g'  harbor.yml
-sed -i 's/private_key: \/your\/private\/key\/path/private_key: ${keypath}/g' harbor.yml
-sed -i 's/port: 80/port: ${port}/g' harbor.yml
+sed -i "s/certificate: \/your\/certificate\/path/certificate: ${crtpath}/g"  harbor.yml
+sed -i "s/private_key: \/your\/private\/key\/path/private_key: ${keypath}/g" harbor.yml
+sed -i "s/port: 80/port: ${port}/g" harbor.yml
 
 ./prepare
 ./install.sh
