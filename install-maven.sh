6a26dd13c19b3852a15d9bd1ba980ec8bf620f6d
myzb <mc1169502544@outlook.com>
2022-01-05 15:53:10 +0800

    alter mvn

--- install-maven.sh
+++ install-maven.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=3.2.0
+version=3.8.4
 wget wget --no-check-certificate https://dlcdn.apache.org/maven/maven-3/${version}/binaries/apache-maven-${version}-bin.tar.gz
 
 tar -zxvf apache-maven-${version}-bin.tar.gz -C /usr/local
